ee089e3bd4d834451e9538a2aac7ad02f9cab6e9
Andreas <ak@a-d-k.de>
2016-09-25 23:04:24 +0200

    Version 1.2
    -create html nodes (mischung von innerHTML + cE führt sonst zu fehlern)
    -fülle transparente Stellen mit weiß
    -clear timer
    -code cleaning

--- mypiclaser.js
+++ mypiclaser.js
@@ -10,7 +10,7 @@
 
 
 var akPicToLaser=function(zielID){
-	var version="1.1 2016-09-25";
+	var version="1.2 2016-09-25";
 	
 	var ziel;	
  
@@ -19,10 +19,11 @@ var akPicToLaser=function(zielID){
 	//http://linuxcnc.org/docs/html/gcode.html
  
 	//helper
-	var cE=function(ziel,e,id,cn){
+	var cE=function(ziel,e,id,cn,innertext){
 			var newNode=document.createElement(e);
 			if(id!=undefined && id!="")newNode.id=id;
 			if(cn!=undefined && cn!="")newNode.className=cn;
+			if(innertext!=undefined && innertext!="")newNode.innerHTML=innertext;
 			if(ziel)ziel.appendChild(newNode);
 			return newNode;
 		}
@@ -76,85 +77,97 @@ var akPicToLaser=function(zielID){
 	var pauseButt;
  
 	var input_Width=undefined;
-	var input_Height;
+	var input_Height=undefined;
  
 	var objektdata={
-		feedrateburn: 800,		//max:2400  800@8%
+		feedrateburn:800,		//max:2400  800@8% 1000@8%
 		feedratemove:2400,
-		minGrau:128,		//0..255  alles unter minGrau wird zu 0
+		minGrau:128,		//0..255  alles unter minGrau wird zu 0 (nicht lasern)
 		width:1,	//mm
 		height:1,
 		unit:"mm",
 		Dlaser:0.125,//Laserdurchmesser in mm  --> minimaler Zeilenabstand -->max 203,2dpi
-		dpi:75		 //Punkte pro Zoll = Punkte pro 2,54cm
+		dpi:75,		 //Punkte pro Zoll = Punkte pro 2,54cm
+	
+		timer:undefined,
+		stopconvert:false
 	}
  
-	var pause=false;
-	//
+	var oInputNr=function(ziel,id,inivalue,min,max){
+		var htmlNode;
+		var value=inivalue;
+		
+		var create=function(){
+			htmlNode=cE(ziel,"input",id);
+			htmlNode.name=id;
+			htmlNode.type="number";
+			htmlNode.step="any";
+			htmlNode.min=min;
+			htmlNode.max=max;
+			//htmlNode.value=inivalue;
+			//htmlNode.placeholder ="in mm";
+			htmlNode.setAttribute("value",value);
+			
+			htmlNode.onchange=function(e){
+				value=this.value;
+			}
+		}
+		
+		this.getvalue=function(){
+			return value;
+		}
+		this.setvalue=function(v){
+			value=v;
+			htmlNode.value=v;
+			htmlNode.setAttribute("value",value);
+		}
+		
+		create();
+	}
 	
 	var ini=function(){
 		var html,p;
+		
 		p=cE(ziel,"p");
-		p.innerHTML="Bitte Datei wählen: ";
+		html=cE(p,"span",'','',"Bitte Datei wählen: ");
+				
 		inputFile=cE(p,"input","inputFile");
 		inputFile.type="file";
 		inputFile.accept="image/*;capture=camera";
 		inputFile.size="50";//MB
 		inputFile.onchange=handleFile;
 		
-		inputimage=cE(ziel,"img","inputimage");
+		inputimage=cE(ziel,"img","inputimage","unsichtbar");
 		inputimage.onload=prework;
-		addClass(inputimage,"unsichtbar");
 		
-		p=cE(ziel,"p","setdaten");
-		addClass(p,"unsichtbar");
-		p.innerHTML="Lasern in einer Größe von ";
+		p=cE(ziel,"p","setdaten","unsichtbar");
+		html=cE(p,"span",'','',"Lasern in einer Größe von ");
 		
+		input_Width=new oInputNr(p,"input_Width",objektdata.width,1,500);
+				
+		html=cE(p,"span",'',''," * ");
 		
-		input_Width=cE(p,"input","input_Width");
-		input_Width.name="input_Width";
-		input_Width.type="number";
-		input_Width.step="any";
-		input_Width.min=1;
-		input_Width.max=500;
-		input_Width.placeholder ="in mm";
-		input_Width.setAttribute("value",objektdata.width);
+		input_Height=new oInputNr(p,"input_Height",objektdata.height,1,500);
+											
+		html=cE(p,"span",'',''," (Breite * Höhe in "+objektdata.unit+") ")
 		
-		p.innerHTML+=" * ";
-		
-		input_Height=cE(p,"input","input_Height");
-		input_Height.name="input_Height";
-		input_Height.step="any";
-		input_Height.type="number";
-		input_Height.placeholder ="in "+objektdata.unit;
-		input_Height.min=1;
-		input_Height.max=500;
-		input_Height.setAttribute("value",objektdata.height);
-									
-		p.innerHTML+=" (Breite * Höhe in "+objektdata.unit+") ";
-		
-		html=cE(p,"a",undefined,"button");
+		html=cE(p,"a",undefined,"button","set new size");
 		html.href="#";
 		html.onclick=setNewSize;
-		html.innerHTML="set new size";
 		
 		
 		p=cE(ziel,"p");
 		outputcanvas=cE(p,"canvas","outputcanvas");
 		
 		p=cE(ziel,"p");
-		makeButt=cE(p,"a","makeButt","button");
+		makeButt=cE(p,"a","makeButt","button unsichtbar","konvertiere");
 		makeButt.href="#";
 		makeButt.onclick=function(){ konvertiere(); return false;}
-		makeButt.innerHTML="konvertiere";
-		addClass(makeButt,"unsichtbar");
- 
+		
 		p=cE(ziel,"p");
-		pauseButt=cE(p,"a","pauseButt","button bred");
+		pauseButt=cE(p,"a","pauseButt","button bred unsichtbar","stopp");
 		pauseButt.href="#";
-		pauseButt.onclick=function(){ pause=true; return false;}
-		pauseButt.innerHTML="stopp";
-		addClass(pauseButt,"unsichtbar");
+		pauseButt.onclick=function(){ objektdata.stopconvert=true; return false;}
 		
 		outPutDoc=cE(ziel,"textarea","outPutDoc");
  		addClass(outPutDoc,"unsichtbar");
@@ -172,12 +185,9 @@ var akPicToLaser=function(zielID){
  		objektdata.width	=this.width /dpi*2.54*10;
 		objektdata.height	=this.height/dpi*2.54*10;
  
-		e=gE("input_Width");
-		e.value=maF(objektdata.width);
+		input_Width.setvalue (maF(objektdata.width));
+		input_Height.setvalue(maF(objektdata.height));
 		
-		e=gE("input_Height");
-		e.value=maF(objektdata.height);
-
 		c=gE("setdaten");
 		subClass(c,"unsichtbar");
 		
@@ -185,11 +195,8 @@ var akPicToLaser=function(zielID){
 	}
 	
  	var setNewSize=function(e){
-		var e;
-		e=gE("input_Width");
- 		objektdata.width	=e.value;
-		e=gE("input_Height");
-		objektdata.height	=e.value;
+		objektdata.width	=input_Width.getvalue();
+		objektdata.height	=input_Height.getvalue();
 
 		preWorkPicture();
 		
@@ -200,7 +207,7 @@ var akPicToLaser=function(zielID){
 	
 
 	var preWorkPicture=function(){		
-		var c,cc,bb,hh,imgd,pix,v,r,g,b,d,x,y,e;
+		var c,cc,bb,hh,imgd,pix,v,r,g,b,alpha,d,x,y,e;
 		var dpi=objektdata.dpi;//Punkte pro Zoll = 2,54cm
  
 		var inputimage=gE("inputimage");
@@ -221,23 +228,30 @@ var akPicToLaser=function(zielID){
 		for(y=0;y<c.height;y++)
 			for(x=0;x<c.width;x++){
 				d=(x*4)+(y)*c.width*4;
+				
 				r=Math.round(16/255*pix[d+0])*16;
 				g=Math.round(16/255*pix[d+1])*16;
 				b=Math.round(16/255*pix[d+2])*16;
- 
+				alpha=pix[d+3];
+				 
 				v=Math.round(r*0.299+g*0.587+b*0.114);	// nach helligkeit
 				v=Math.floor((r+g+b)/3);				// (r+g+b)/2
 				
+				if(alpha<255){//transparente stellen(gif,png) mit weiß füllen
+					alpha=255; 
+					pix[d+3]=alpha;
+					if(v==0)v=255;
+					}
+					
 				if(v>objektdata.minGrau)v=255;
 				
-				pix[d+0]=v;
+				pix[d+0]=v;//alle Farbkanäle mit grauwert befüllen
 				pix[d+1]=v;
 				pix[d+2]=v;
 				
 			}
 		 cc.putImageData(imgd, 0, 0);
- 		
-	}
+	};
  
  
 	var setPixel=function(canv,x,y,r,g,b,a){
@@ -250,14 +264,14 @@ var akPicToLaser=function(zielID){
 		d[2]   = b;
 		d[3]   = a;
 		cc.putImageData( id, x, y );   
-	}
+	};
 	var getPixel=function(canv,x,y){
 		var cc,id,d;
 		cc=canv.getContext("2d");
 		id=cc.getImageData(x,y,1,1);
 		pix=id.data;
 		return pix;//[r,g,b,a]
-	}
+	};
  
  
 	var zeile=0;
@@ -266,16 +280,19 @@ var akPicToLaser=function(zielID){
 		
 		addClass(makeButt,"unsichtbar");
 		subClass(pauseButt,"unsichtbar");
+		
+		outPutDoc.style.display="none";
+		
 		outPutDoc.innerHTML=" ; start v"+version+"\n";
 		outPutDoc.innerHTML+=" ; "+maF(objektdata.width)+" x "+maF(objektdata.height)+"mm² \n";
 		
 		outPutDoc.innerHTML+="G90 ;absolute Positioning\n";
 		outPutDoc.innerHTML+="M08 ;Flood Coolant On\n";// opt.
 		outPutDoc.innerHTML+="G21 ;Set Units to Millimeters\n";// 
-		//outPutDoc.innerHTML+="F"+objektdata.feedrateburn+" ;speed\n"; //speed hängt an G
-		outPutDoc.style.display="none"
-		window.setTimeout(konvertiereF,10);//Zeilen per Timer durchgehen um Script-Blockierung zu verhindern
-	}
+				
+		if(objektdata.timer!=undefined)window.clearTimeout(objektdata.timer);
+		objektdata.timer=window.setTimeout(konvertiereF,10);//Zeilen per Timer durchgehen um Script-Blockierung zu verhindern
+	};
  
  
 	var konvertiereF=function(){
@@ -299,25 +316,28 @@ var akPicToLaser=function(zielID){
 		y=zeile;//Y per Timer sonst Script zu sehr ausgelastet
 		
 		szeile="G1 X"+maF(lposX)+" Y"+maF(lposY)+" S0 F"+objektdata.feedratemove+"\n";	//erste Position anfahren  //TODO: testen mit S0 evtl. m3/m5 nicht nötig
-		//szeile+="F"+objektdata.feedrateburn+"\n";
 		szeile+="M3\n";										//Spindle On, Clockwise
 		
 		valuecount=0;
 		x=0;
 		d=(x*4)+(y)*c.width*4;
-		var lastpixel=pix[d+1];
+		var getkanal=0;//rgba 0=r
+		var lastpixel=pix[d+getkanal];
 		var minblack=255;
 		var lastbefehl="";
 		
 		for(x=1;x<c.width;x++){
-			d=(x*4)+(y)*c.width*4;
-			r=pix[d+1];//nur Rotkanal auswerten (=graustufen 0...255)
+			d=(x*4)+(y)*c.width*4; //pos=rgba * y*width*rgba
+			r=pix[d+getkanal];//nur einen kanal auswerten (=graustufen 0...255)
 			
 			//lauflängen...
 			if(r!=lastpixel || x==(c.width-1)){
 				//if(x==(c.width-1) && lastpixel!=255)
-				//TODO: leerfahrten am Ende evtl. entfernen
+				
 				//burnIN am Anfang/Ende verhindern, wie? -> wenigerPower + langsammer?
+				//-->liegt an grbl -->CNC-Fräse langsam anfahren/abbremsen
+				
+				//G1 Xnnn Ynnn Znnn Ennn Fnnn Snnn 
 				lastbefehl="G1 X"+maF(lposX);//fahre bis  	//G0 Rapid Move: quickly and efficiently as possible  
 															//G1 Controlled Move: as precise as possible
 				if(lastpixel==255)
@@ -327,7 +347,7 @@ var akPicToLaser=function(zielID){
 				
 				lastbefehl+=" S"+Math.floor(1000-(1000/255*lastpixel))+"\n";	//Set Spindle Speed/Intensität
 			
-				if( x==(c.width-1) ){
+				if( x==(c.width-1) ){//leerfahrten am Ende entfernen
 					if(lastpixel<255)
 						szeile+=lastbefehl;
 				}
@@ -336,18 +356,18 @@ var akPicToLaser=function(zielID){
 				}
 				
 				valuecount++;
-				if(minblack>lastpixel)minblack=lastpixel;
-				//G1 Xnnn Ynnn Znnn Ennn Fnnn Snnn 
+				if(minblack>lastpixel){
+					minblack=lastpixel;
+					}
 				
-				lastpixel=r;
+				lastpixel=r;				
 			}	
 			
 			lposX+=stepX;				
 		}
-		//szeile+="\n";
-		//TODO: wenn Zeile =0 dann gleich zur nächsten
 		szeile+="M5\n"; //Spindle Off
 		
+		//wenn Zeile =0 dann gleich zur nächsten
  		if(valuecount>1 && minblack<255){//keine Leerzeilen erzeugen
 			outPutDoc.innerHTML+=szeile;
 			
@@ -366,8 +386,7 @@ var akPicToLaser=function(zielID){
  
 		zeile++;
 		if(zeile<c.height){
-				if(pause){//Stopp
-					//window.setTimeout(konvertiereF,1000);//1sec
+				if(objektdata.stopconvert){//Stopp
 					outPutDoc.style.display="inline-block";	
 				}
 				else
@@ -382,12 +401,11 @@ var akPicToLaser=function(zielID){
 				outPutDoc.style.display="inline-block";	
 				addClass(pauseButt,"unsichtbar");				
 			}
-	}
- 
- 
+	};
+  
 	var handleFile=function(){
 		addClass(pauseButt,"unsichtbar");
-		pause=false;
+		objektdata.stopconvert=false;
  
 		var reader = new FileReader();
 		reader.onload = function(theFile) {
@@ -404,22 +422,15 @@ var akPicToLaser=function(zielID){
  
 					loadImage(ifile);
 					subClass(makeButt,"unsichtbar");
-				}
- 
+				}; 
 				outPutDoc.innerHTML="";
- 
-				
- 
 			};
 		reader.readAsBinaryString(inputFile.files[0]);		
-	}
- 
+	}; 
  
 	ziel=gE(zielID);
 	ini();
-	
-	
-}
+};
  
 
  
